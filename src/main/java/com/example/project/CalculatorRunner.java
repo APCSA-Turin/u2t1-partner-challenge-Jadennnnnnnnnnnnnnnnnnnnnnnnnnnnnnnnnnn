8c640d6a8799af258ac80c3eee4b7caac538684f
Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <168836666+Jadennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2024-09-24 13:14:07 +0000

    commit

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -2,11 +2,10 @@ package com.example.project;
 
 public class CalculatorRunner {
     public static void main(String[] args) {
-
-        //test your program here
-        //1. create an instance of the calculator class
-
-        //2. call any methods of that class to test
-
+        Calculator calculator = new Calculator("TI84");
+        System.out.println(calculator.performOperation("+", 10, -5));
+        System.out.println(calculator.divisibleBy(8, 2));
+        System.out.println(calculator.coordinatePair(2,3));
+        System.out.println(calculator.info());
     }
 }
